a3b48a03f0d082f8e99e682ae1a99396626c1fca
Estudiantes <Estudiantes@Labing50613.udistrital.edu.co>
2017-03-15 16:48:42 -0500

    ACCIONES CLASE AHORCADO Y DISEÑO TABLERO

---
+++ Ahorcado/src/ahorcado/Ahorcado.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahorcado;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Ahorcado {
+    
+    char letras;
+    int vidas=8;
+    
+    char ingresarLetras(){
+        
+        return 0;
+        
+    }
+    void verificarLetras(){
+
+   
+}
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
